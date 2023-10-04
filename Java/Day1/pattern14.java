c6ed54ea064ae371e52577502bb7c27d79ef6ac9
Itzshine <harshdambhareh53@gmail.com>
2023-10-04 19:30:24 +0530

    pattern day1

--- Java/Day1/pattern14.java
+++ Java/Day1/pattern14.java
@@ -1,13 +1,14 @@
-public class pattern5 {
+public class pattern14 {
     public static void main(String[] args) {
         int row = 5;
-        int col = 5;
+        int col = 1;
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                System.out.print("*");
+                char c = (char)('A'+j);
+                System.out.print(c);
             }
-            col--;
             System.out.println();
+            col++;
         }
     }
 }
---
+++ Java/Day1/pattern19.java
@@ -0,0 +1,38 @@
+public class pattern19 {
+    public static void main(String[] args) {
+    int row = 5;
+    int col = 5;
+    int space = 0;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col-1;j++){
+            System.out.print("*");
+        }
+     
+        for(int j=0;j<space;j++)System.out.print(" ");
+
+        for(int j=0;j<col-1;j++){
+            System.out.print("*");
+        }
+       if(i<row-2) System.out.println("");
+        space+=2;
+        col--;
+    }
+    row = 5;
+    col++;
+    space-=2;
+        for(int i=0;i<row;i++){
+        for(int j=0;j<col-1;j++){
+            System.out.print("*");
+        }
+     
+        for(int j=0;j<space;j++)System.out.print(" ");
+
+        for(int j=0;j<col-1;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+        space-=2;
+        col++;
+    }
+    }
+}
